256d4b9f665c772ee4096d9492eb3818c3db20c5
danizaghian <dani.zaghian@gmail.com>
2019-05-09 11:06:19 -0700

    adds query solutions and segment parameter

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -7,4 +7,32 @@ class Api::ParamsExamplesController < ApplicationController
 		render 'query_params.json.jbuilder'
 	end
 
+	def query_params_name
+		my_name = params[:name]
+		@caps_name = my_name.upcase
+		@message = "Your name is #{@caps_name}"
+		if @caps_name.starts_with?("A")
+			@message = "Hey, your name starts with A!"
+		end
+		render 'query_params_name.json.jbuilder'
+	end
+
+	def guess_query_action
+		winning_number = 32
+		user_guess = params[:guess].to_i
+		if user_guess > winning_number
+			@message = "Pick lower!"
+		elsif user_guess < winning_number
+			@message = "Pick higher!"	
+		else
+			@message = "You win!"	
+		end
+		render 'guess_query.json.jbuilder'
+	end
+
+	def segment_params_action
+		@message = params[:wildcard]
+		render 'segment_params.json.jbuilder'
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/query_params_url" => "params_examples#query_params_action"
+
+    get "/query_params_name" => "params_examples#query_params_name"
+
+    get "/guess_query_url" => "params_examples#guess_query_action"
+
+    get "/segment_params_url/:wildcard" => "params_examples#segment_params_action"
   end
 
 end
